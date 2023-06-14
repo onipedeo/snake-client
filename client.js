a9b491c0c6e6f0d55810f83a2910d14c1ac3d13e
Tobi Onipede <onipede89@gmail.com>
2023-06-14 23:21:38 +0100

    experiment with move commands

--- client.js
+++ client.js
@@ -9,14 +9,15 @@ const connect = function() {
 
   //interpret incoming data as text
   conn.setEncoding("utf8");
-
+  //feedback from server
   conn.on("data", (messageFromServer) => {
     console.log("message from Server: ", messageFromServer);
   });
-
+  //tasks to do on connection to the server
   conn.on("connect", () => {
     console.log("connection successful to the game server");
-    conn.write("Name: LDC");
+    conn.write("Name: LDC");//sends my initial to the server
+
   });
 
   return conn;
