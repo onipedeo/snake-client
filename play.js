e4f4c742222dd9f73a50b6c032ac445ddb6d2b8f
Tobi Onipede <onipede89@gmail.com>
2023-06-15 04:48:32 +0100

    create input.js and refactored play.js

--- play.js
+++ play.js
@@ -1,20 +1,6 @@
 const { connect } = require("./client");
+const { setUpInput } = require("./input");
 
-//Handling input from the keyboard
-const setUpInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-}
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 console.log("connecting...");
 connect();
 setUpInput();
