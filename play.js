0429d7fb1d44f64cc972e6da2f4436cbf5098a67
Tobi Onipede <onipede89@gmail.com>
2023-06-15 04:39:32 +0100

    implement cancelling the conection in the terminal with ^C

--- play.js
+++ play.js
@@ -1,4 +1,20 @@
 const { connect } = require("./client");
 
+//Handling input from the keyboard
+const setUpInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 console.log("connecting...");
 connect();
+setUpInput();
